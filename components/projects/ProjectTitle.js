440d55c4e9a5449e8bd430f6d64a8b3bfcc8bd31
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2022-04-28 11:04:26 -0400

    sticky project title

--- components/projects/ProjectTitle.js
+++ components/projects/ProjectTitle.js
@@ -4,5 +4,9 @@ import { useRouter } from "next/router";
 import CardVideo from "../CardVideo";
 
 export default function ProjectTitle(props) {
-  return <p className="grid col-start-1 text-white">{props.children}</p>;
+  return (
+    <p className="grid col-start-1 col-end-5 md:sticky md:top-[2rem] text-white">
+      {props.children}
+    </p>
+  );
 }
