8aac3e1ee0fc6a079af8d873e5e0d102bff1723e
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2022-06-03 16:47:15 +0800

    add metadata previews

--- components/Meta.js
+++ components/Meta.js
@@ -9,12 +9,45 @@ function Meta() {
         name="viewport"
         content="width=device-width, initial-scale=1, viewport-fit=cover"
       />
+
+      {/* Primary Meta Tag */}
+      <title>Joseph Zhang – Interaction Designer</title>
+      <meta name="title" content="Joseph Zhang – Interaction designer" />
       <meta
-        key="description"
         name="description"
-        content="Interaction designer"
+        content="Joseph is a designer interested in authoring tools and multimodal interaction. "
+      ></meta>
+
+      {/* Open Graph/FB */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Joseph Zhang – Interaction designer" />
+      <meta
+        property="og:description"
+        content="Joseph is a designer interested in authoring tools and multimodal interaction. "
       />
-      <title key="title">Joseph Z</title>
+      <meta
+        property="og:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta
+        property="twitter:title"
+        content="Joseph Zhang – Interaction designer"
+      />
+      <meta
+        property="twitter:description"
+        content="Joseph is a designer interested in authoring tools and multimodal interaction. "
+      />
+      <meta
+        property="twitter:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+
+      {/* Favicon & fonts */}
       <link key="favicon" rel="icon" href="/favicon.jpg" importance="low" />
       <link key="apple-touch-icon" rel="apple-touch-icon" href="/favicon.jpg" />
       <link rel="stylesheet" href="https://use.typekit.net/liy8bpw.css"></link>
