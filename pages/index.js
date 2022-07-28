d4f7ea07a2876600dfe20ae1604bdf513130240c
Joseph <jxsephz@gmail.com>
2022-07-28 12:21:02 -0700

    update copy

--- pages/index.js
+++ pages/index.js
@@ -41,26 +41,24 @@ export default function Home() {
             scrollY > 40 ? "md:opacity-0 pointer-events-none" : ""
           } gap-6 text-zinc-100 lg:sticky top-[2rem] pt-2  mb-8 md:mt-0 md:columns-2 col-start-1 col-end-13 md:col-start-5 md:col-end-13 lg:col-start-5 lg:col-end-13 transition duration-[200ms] ease-[cubic-bezier(0.22, 1, 0.36, 1)]`}
         >
-          Joseph is a designer interesting in digital toolmaking. After working at <TextLink link="https://www.apple.com/siri/">Apple</TextLink>,{" "} he discovered a passion for small, passionate teams and have since worked with high-functioning starts-ups like {" "}
+          Joseph is an interaction designer interested in digital toolmaking and multimodal input experiences. After working at <TextLink link="https://www.apple.com/siri/">Apple</TextLink>,{" "} he discovered a love for small, high-caliber teams and have since worked with starts-ups like {" "}
           <TextLink link="https://natural.ai/#/">Brain Technologies</TextLink>, {" "}
           <TextLink link="https://azuki.com">
             Azuki
           </TextLink>, and now <TextLink link="https://twitter.com/skiffprivacy">Skiff</TextLink>
-          . He believes good design reflects artistic values focused
-          around emotion and play. He holds a{` `}
+          . Most recently, he graduated from Carnegie Mellon University studying {" "}
           <TextLink link="https://design.cmu.edu/content/bachelor-design">
-            Bachelor of Design
+            Communication Design
           </TextLink>{" "}
           and{" "}
           <TextLink link="https://www.hcii.cmu.edu/">
             Human-Computer Interaction
-          </TextLink>{" "}
-          minor from Carnegie Mellon University. Joseph loves meeting new faces so come say hello ~ 
+          </TextLink>. Joseph doesn't actually speak in third-person, so if you're in Seattle or the Bay Area, don't be afraid to say hi. 
         </motion.p>
 
         <ProjectCard
           title="Skiff"
-          description="productivity tools"
+          description="collaboration tools"
           col={1}
           locked={true}
           isImage={true}
