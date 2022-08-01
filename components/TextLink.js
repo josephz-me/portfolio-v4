91df99a492bd7495a0f1a39a0e97db8131a892cd
Joseph Zhang <jxsephz@gmail.com>
2022-08-01 12:15:13 -0700

    dotted lines

--- components/TextLink.js
+++ components/TextLink.js
@@ -1,7 +1,7 @@
 export default function TextLink(props) {
   return (
     <a
-      className="bg-white bg-opacity-[.05] hover:bg-opacity-10 px-[2px] pt-[2px] border-b-[1px] border-solid border-[rgba(255,255,255,.2)] hover:border-[rgba(255,255,255,.4)]"
+      className="border-dotted bg-white bg-opacity-[.05] hover:bg-opacity-10 px-[2px] pt-[2px] border-b-[1px] border-solid border-[rgba(255,255,255,.2)] hover:border-[rgba(255,255,255,.4)]"
       target="_blank"
       rel="noreferrer"
       href={props.link}
