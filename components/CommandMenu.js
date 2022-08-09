82e8d36c7ecaff3ed3b6d09c85644c4c503e9e1d
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2022-08-09 10:42:51 -0700

    add locks and disabled projects to site

--- components/CommandMenu.js
+++ components/CommandMenu.js
@@ -103,6 +103,12 @@ export default function CommandMenu() {
                   <Item closeModal={closeModal} href="metalink" icon="metalink">
                     Metalink
                   </Item>
+                  <Item closeModal={closeModal} icon="skiff">
+                    Skiff
+                  </Item>
+                  <Item closeModal={closeModal} icon="azuki">
+                    Azuki
+                  </Item>
                 </Command.Group>
                 <Command.Group heading="Socials">
                   <ExternalItem
@@ -143,16 +149,32 @@ const Item = ({ closeModal, href, children, icon }) => {
       <Link href={href ? href : ""}>
         <a
           onClick={closeModal}
-          className="flex gap-4 items-center px-[20px] py-1 w-full"
+          className="flex justify-between items-center px-[20px] py-1 w-full"
         >
-          {icon !== undefined && (
-            <img
-              src={`/project-icons/${icon}.svg`}
-              alt={`${icon} icon`}
-              className={`w-10 h-10 rounded-xl inline-block`}
-            />
+          <div className="flex gap-4 items-center">
+            {icon !== undefined && (
+              <img
+                src={`/project-icons/${icon}.svg`}
+                alt={`${icon} icon`}
+                className={`w-10 h-10 rounded-xl inline-block`}
+              />
+            )}
+            <p>{children}</p>
+          </div>
+          {!href && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clipRule="evenodd"
+              />
+            </svg>
           )}
-          <p>{children}</p>
         </a>
       </Link>
     </Command.Item>
