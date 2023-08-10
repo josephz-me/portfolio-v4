326f4b30f8595d7794cf891940bfa4e07cc26434
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2023-08-10 11:12:29 -0700

    save

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import { BookList, DialogTrigger, DialogRoot } from "./BookList";
 const GLOBAL_SPACING = "text-padding pb-10 pt-6 md:pb-10 md:pt-8";
 const BREAKPOINT = "col-start-1 col-end-13";
+import Image from "next/image";
+import Star from "../public/star.svg";
 
 export default function Footer() {
   const [open, setOpen] = React.useState(false);
@@ -77,15 +79,12 @@ export default function Footer() {
           <p>
             <DialogRoot open={open} setOpen={setOpen}>
               <DialogTrigger>
-                <div className="!absolute game-border bottom-6 right-6 p-4 hover:bg-zinc-700 hover:scale-[1.05]  bg-zinc-800 h-fit w-fit rounded-full shadow-2xl border border-solid border-white/10">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-6 h-6 "
-                  >
-                    <path d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z" />
-                  </svg>
+                <div className="!absolute bottom-6 justify-items items-center right-6 rounded-full shadow-2xl p-3 hover:bg-zinc-700 hover:scale-[1.05] bg-zinc-800 border border-solid border-white/10 game-border">
+                  <img
+                    src={`/star.svg`}
+                    alt="book star icon"
+                    className="w-6 h-6 fill-white"
+                  />
                 </div>
               </DialogTrigger>
               <BookList open={open} />
