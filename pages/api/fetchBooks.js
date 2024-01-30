9fa04c6f9e7785d896e8d0faefe40f3d36c75775
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-01-30 00:38:01 -0800

    next public

--- pages/api/fetchBooks.js
+++ pages/api/fetchBooks.js
@@ -1,10 +1,10 @@
 const { Client } = require('@notionhq/client');
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_API_KEY });
 
 export default async (req, res) => {
   const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
+    database_id: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID,
   });
 
   res.status(200).json(response.results);
