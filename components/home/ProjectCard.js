d1e1a8cf9fe689230bb20efc9c8a21d08998b4b9
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-04-16 00:28:07 -0700

    save

--- components/home/ProjectCard.js
+++ components/home/ProjectCard.js
@@ -15,7 +15,7 @@ export default function ProjectCard({ isVideo = false, ...props }) {
       variants={props.animation}
       href={`${props.link}`}
     >
-      <div className="relative game-border">
+      <div className="relative game-border group-hover:opacity-80">
         <Media isVideo={isVideo} src={props.content} responsive />
 
         {props.notionLabel && (
@@ -28,7 +28,7 @@ export default function ProjectCard({ isVideo = false, ...props }) {
             target="_blank"
             className=" transition duration-200 border border-black/20 border-solid w-fit flex items-center gap-2 absolute bottom-4 z-20 right-[-50%] left-[50%] translate-x-[-50%] px-3 py-1 bg-white text-black rounded-full"
           >
-            <p className="body tracking-normal text-xs uppercase font-bold">
+            <p className="body uppercase tracking-normal text-xs">
               Acquired by Notion
             </p>
 
@@ -45,7 +45,7 @@ export default function ProjectCard({ isVideo = false, ...props }) {
         <h1 className={`caption text-zinc-50`}>{props.title}</h1>
         <p className="caption text-zinc-500">{props.description}</p>
         {props.locked && (
-          <p className="absolute text-neutral-100 rounded-md right-4 top-4 px-2 z-10 py-1 bg-gray-800 shadow-xl">
+          <p className="caption absolute text-neutral-100 rounded-md right-4 top-4 px-2 z-10 py-1 bg-gray-800 shadow-xl">
             LOCKED
           </p>
         )}
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Compose from '../public/work/skiff/compose-modal.jpg';
 import Apple from '../public/project-covers/apple-v2.jpg';
 import Divider from '../components/projects/Divider';
 import Brain from '../public/project-covers/natural.jpg';
-import Azuki from '../public/project-covers/azuki-2.jpg';
+import Azuki from '../public/project-covers/azuki.jpg';
 import CollectorStatus from '../public/project-covers/collector-status.jpg';
 import Cursor from '../public/project-covers/cursor.jpg';
 import AIGA from '../public/project-covers/aiga-square.jpg';
@@ -16,6 +16,7 @@ import Metalink from '../public/project-covers/metalink.jpg';
 import SkiffIcons from '../public/project-covers/skiffIcons.jpg';
 import Experiments from '../public/work/experiments/LE-1.jpg';
 import FormatBar from '../public/work/skiff/format-bar.jpg';
+import Plane from '../public/project-covers/plane.jpg';
 
 const gapValue = 'gap-6';
 
@@ -25,10 +26,10 @@ export default function Home() {
       <GridContainer>
         {/* ABOUT SECTION */}
         <section className="grid-layout my-6">
-          <div className="col-start-1 col-end-13 md:col-end-5 gap-3 flex flex-col">
+          <div className="col-start-1  col-end-13 md:col-end-5 gap-3 flex flex-col">
             <p className="caption opacity-40 text-white">ABOUT</p>
             <p
-              className={`body text-white z-1000 col-start-1 col-end-13 md:col-start-1 md:col-span-5 mb-4 md:mb-0 opacity-80`}
+              className={`body text-white z-1000 col-start-1 col-end-13 md:col-start-1 md:col-span-5 mb-4 md:mb-0`}
             >
               Jo (He/Him) designs interfaces. He thrives in complex, ambiguous
               problem spaces focused around interactive media, digital tooling,
@@ -120,12 +121,6 @@ export default function Home() {
             />
 
             <ProjectCard
-              title="Skiff Pages"
-              description="Editor Tool"
-              link="/skiff/#editor"
-              content={FormatBar}
-            />
-            <ProjectCard
               title="Thinkspace"
               description="Assistive AI Agents"
               link="/thinkspace"
@@ -138,6 +133,12 @@ export default function Home() {
               link="/skiff/#icons"
               content={SkiffIcons}
             />
+            <ProjectCard
+              title="LIGHT ENGINE"
+              description="Industrial Design"
+              link="/lightengines"
+              content={Experiments}
+            />
           </div>
           {/* COLUMN 2 */}
           <div className={`flex flex-col ${gapValue}`}>
@@ -148,22 +149,22 @@ export default function Home() {
               content={CollectorStatus}
             ></ProjectCard>
             <ProjectCard
+              title="Plane"
+              description="Productivity"
+              locked
+              content={Plane}
+            />
+            <ProjectCard
+              title="Skiff Pages"
+              description="Editor Tool"
+              link="/skiff/#editor"
+              content={FormatBar}
+            />
+            <ProjectCard
               title="AIGA Hue"
               description="Data Visualization"
               link="/aiga"
               content={AIGA}
-            />
-            <ProjectCard
-              title="LIGHT ENGINE"
-              description="Industrial Design"
-              link="/lightengines"
-              content={Experiments}
-            ></ProjectCard>
-            <ProjectCard
-              title="Azuki Sisters"
-              description="Product"
-              link="/azuki/#sisters"
-              content={Sisters}
             />
           </div>
 
@@ -177,6 +178,13 @@ export default function Home() {
               content={'/project-covers/apple.mp4'}
             />
             <ProjectCard
+              title="Brain Technologies"
+              description="Conversational AI"
+              link="/brain"
+              content={Brain}
+            />
+
+            <ProjectCard
               title="Skiff Mail"
               description="Productivity Tooling"
               link="/skiff/#mail"
@@ -188,12 +196,6 @@ export default function Home() {
               link="/pixelpush"
               isVideo
               content={'/project-covers/pixel-push.mp4'}
-            />
-            <ProjectCard
-              title="Brain Technologies"
-              description="Conversational AI"
-              link="/brain"
-              content={Brain}
             />
 
             <ProjectCard
@@ -212,12 +214,12 @@ export default function Home() {
 function WorkExperience(props) {
   return (
     <>
-      <h1 className="opacity-80 col-start-1 col-end-3 col-span-2 text-white body">
+      <h1 className=" col-start-1 col-end-3 col-span-2 text-white body">
         {props.title}
         <br />
         {props.date}
       </h1>
-      <p className="opacity-80 text-white body col-start-3 col-span-4">
+      <p className=" text-white body col-start-3 col-span-4">
         {props.children}
       </p>
     </>
