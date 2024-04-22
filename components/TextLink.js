4fc393a149e55f83afa588f6e8697ea4cd21bcc1
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-04-21 22:43:21 -0700

    update content

--- components/TextLink.js
+++ components/TextLink.js
@@ -3,7 +3,7 @@ export default function TextLink(props) {
   return (
     <>
       <a
-        className="cursor-pointer border-dotted bg-white/[.05] hover:bg-yellow-300/10 px-[2px] hover:text-yellow-300 pt-[2px] border-b-[1px] border-solid border-[rgba(255,255,255,.5)] hover:border-yellow-300"
+        className="cursor-pointer border-dotted hover:bg-yellow-300/10 px-[2px] hover:text-yellow-300 pt-[2px] dotted-underline"
         target={props.sameTab ? '' : '_blank'}
         rel="noreferrer"
         href={props.link}
