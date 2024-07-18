7509dbbde1d042d1d74b6aaf36f873a762c481f9
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-07-17 23:48:13 -0400

    update body width

--- components/projects/ProjectBody.js
+++ components/projects/ProjectBody.js
@@ -2,16 +2,18 @@ import GridContainer from '../GridContainer';
 import TextLink from '../TextLink';
 import { useRouter } from 'next/router';
 import CardVideo from '../CardVideo';
+import { twMerge } from 'tailwind-merge';
 
 export default function BodyText(props) {
   return (
     <div
       id={props.id}
-      className={`${
+      className={twMerge(
+        'grid-gap col-start-1 col-end-13 mb-2 text-white',
         props.col == 2
           ? 'md:columns-2 md:col-start-5 md:col-end-13'
-          : 'md:col-start-5 md:col-end-13'
-      } grid-gap col-start-1 col-end-13 mb-2 text-white`}
+          : 'md:col-start-5 md:col-end-13 lg:col-end-12'
+      )}
     >
       {props.title !== undefined && (
         <h2 className="mt-4 mb-2 h2">✶ {props.title}</h2>
