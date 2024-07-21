1e890f01b758aa2ecb330ce4a44b7519ea211d4b
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-07-21 15:33:17 -0400

    remove body width animation

--- components/Page.js
+++ components/Page.js
@@ -8,7 +8,7 @@ export default function Page(props) {
       <div className="w-full flex items-center flex-col z-[200]">
         <div className="z-10 w-full h-full bg-[#111111] justify-center flex">
           <div
-            className={`flex relative flex-col px-3 pb-8 w-full max-w-8xl border-b border-solid shadow-xl duration-500 z-[9] text-padding md:px-3 border-neutral-800`}
+            className={`flex relative flex-col px-3 pb-8 w-full border-b border-solid shadow-xl max-w-8xl z-[9] text-padding md:px-3 border-neutral-800`}
           >
             <Meta />
             <Navbar />
