52a9c1794dd0a10183951b58c6826c0bb4b3332f
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2024-08-31 18:18:35 -0700

    remove

--- components/Meta.js
+++ components/Meta.js
@@ -56,14 +56,6 @@ function Meta() {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-7T632FJ4W4"
       ></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-7T632FJ4W4');
-        `}
-      </script>
     </Head>
   );
 }
