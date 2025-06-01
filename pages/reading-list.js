77c8bc673769838027946dcae8189da0f4705097
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2025-06-01 00:18:54 -0400

    fix

--- pages/reading-list.js
+++ pages/reading-list.js
@@ -8,7 +8,7 @@ const gapValue = 'gap-6';
 export async function getStaticProps() {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
+    database_id: process.env.NOTION_READING_ID,
   });
 
   return { props: { notionData: response.results }, revalidate: 1 };
