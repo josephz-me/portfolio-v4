9297363bc456266090d5eb4927fd0417a23df2ad
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2025-07-30 22:59:27 -0400

    add assets

---
+++ components/projects/MobileMockupWithFrame.js
@@ -0,0 +1,16 @@
+export default function MobileMockupWithFrame({ src, className = '' }) {
+  return (
+    <div
+      className={`relative aspect-[16/12] py-12 overflow-hidden bg-white flex items-center justify-center col-end-13 transition duration-300 col-start-1 md:col-start-5 ${className}`}
+    >
+      <div className="relative h-full w-auto">
+        <video className="h-full w-auto rounded-2xl" src={src} autoPlay muted loop />
+        <img
+          alt="iphone frame"
+          src="/iphone-frame.png"
+          className="scale-[1.20] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ pages/animee.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import TextLink from '../components/TextLink';
+import GridContainer from '../components/GridContainer';
+import ProjectHero from '../components/projects/ProjectHero';
+import ProjectBody from '../components/projects/ProjectBody';
+import ProjectMedia from '../components/projects/ProjectMedia';
+import ProjectTitle from '../components/projects/ProjectTitle';
+import ExternalVideo from '../components/projects/ExternalVideo';
+import Spacer from '../components/projects/Spacer';
+import LinkCard from '../components/projects/LinkCard';
+import MobileMockupWithFrame from '../components/projects/MobileMockupWithFrame';
+import Link from 'next/link';
+import ProjectDetails from '../components/projects/ProjectDetails';
+// media
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+  },
+};
+
+export default function Animee() {
+  const [scrollY, setScrollY] = useState(0);
+
+  Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+    return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <main className="min-h-[70vh]">
+      <GridContainer>
+        <ProjectHero isVideo={false} content="/project-covers/aiga.jpg" />
+        <ProjectTitle role="Mobile app">Anime.com</ProjectTitle>
+        <ProjectBody col={2}>
+          AIGA Hue is a desktop web experience that visualizes career data from the 2019 AIGA Design
+          Census in a playful and informative manner. The recent over-saturation of interest in
+          certain design fields has resulted in skewed perceptions of neighboring creative
+          industries. This project strives to break those stereotypes by providing a holistic
+          understanding of the creative industry and all the viable career options that exist.
+        </ProjectBody>
+        <ProjectDetails
+          role={['Frontend Developer', 'Visual Designer']}
+          collaborators={['Kyuha Shim (advisor)', 'Langston Wells', 'Stefanie Suk']}
+          duration="2 months"
+          tools={['HTML/CSS/JS', 'Figma', 'Cinema4D']}
+        />
+        <LinkCard col={1} link="https://josephz-me.github.io/aiga-hue/">
+          View live website
+        </LinkCard>
+        <LinkCard link="https://josephz.notion.site/AIGA-Hue-38cd5f40c38c464ab1c5885468a1a3de">
+          View complete documentation
+        </LinkCard>
+        <Spacer></Spacer>
+        <MobileMockupWithFrame src="/work/animee/navigation.mp4" />
+        <MobileMockupWithFrame src="/work/animee/export.mp4" />
+        <ProjectBody caption={true}>
+          {`Synthesized +9400 data points from AIGA's census and interactively
+          visualizing them through Matter.js, a Javascript physics engine.`}
+        </ProjectBody>
+      </GridContainer>
+    </main>
+  );
+}
