39dfeb27e93e49876c7f611d19afc9da5945f382
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2025-08-02 20:02:53 -0400

    update content

--- components/projects/ProjectHero.js
+++ components/projects/ProjectHero.js
@@ -6,7 +6,7 @@ import Media from '../Media';
 export default function ProjectHero(props) {
   return (
     <Media
-      className="select-none relative overflow-hidden my-2 md:my-8 h-[60vw] md:h-[49vw] object-cover w-full grid col-span-12 border border test"
+      className="select-none relative overflow-hidden my-2 md:my-8 h-[60vw] md:h-[49vw] object-cover w-full grid col-span-12 border border"
       isVideo={props.isVideo}
       src={props.content}
     />
